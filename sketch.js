b847468d66ceacf4712a8d256854f45fbd5103a0
Miles-Ri <71036446+Miles-Ri@users.noreply.github.com>
2020-12-18 13:39:53 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -11,12 +11,12 @@ var form, player, game;
 var cars, car1, car2, car3, car4;
 
 function preload (){
-car1_img = loadImage ("images/car1.png")
-car2_img = loadImage ("images/car2.png")
-car3_img = loadImage ("images/car3.png")
-car4_img = loadImage ("images/car4.png")
-ground_img = loadImage ("images/ground.png")
-track_img = loadImage ("images/track.jpg")
+car1_img = loadImage ("car1.png")
+car2_img = loadImage ("car2.png")
+car3_img = loadImage ("car3.png")
+car4_img = loadImage ("car4.png")
+ground_img = loadImage ("ground.png")
+track_img = loadImage ("track.jpg")
 
 }
 
